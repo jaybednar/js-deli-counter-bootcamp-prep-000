e71fe9f8945afb3e5f7c14e6d25f4fa7f00335e2
Jay Bednar <ironviralmedia@gmail.com>
2018-01-23 03:33:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing() {
-  if (katzDeli.length === 0) {
+  if (katzDeliLine.length === 0) {
     return `There is nobody waiting to be served!`
   } else {
-    return `Currently serving ${katzDeli.shift()}`;
+    return `Currently serving ${katzDeliLine.shift()}`;
   }
 }
