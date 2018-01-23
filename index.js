d029850b8f43e5a0dcdc7969c54a0598316cf9e9
Jay Bednar <ironviralmedia@gmail.com>
2018-01-23 03:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine (katzDeliLine) {
    if (katzDeliLine.length === 0) {
     return `The line is currently empty.`
   } else {
-    for (var i = 0; i < katzDeliLine.length; i++) {
+    for (var i = 1; i < katzDeliLine.length; i++) {
       katzDeliLine[i] = `${i}. ${katzDeliLine[i]}`
     }
     return `Currently serving ${katzDeliLine.join(', ')}.`;
