92db2169b4594640cd327fbeb6bf77c9d997cf2e
Jay Bednar <ironviralmedia@gmail.com>
2018-01-23 03:36:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
+
+function currentLine (katzDeliLine) {
+   if (katzDeliLine.length === 0) {
+    return `There is nobody waiting to be served!`
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+}
